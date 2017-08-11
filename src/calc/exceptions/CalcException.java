ffb97f508f5e16270800a0d06d258c841d5b0ff4
Bernardo Esteves <b_esteves61@hotmail.com>
2017-08-11 19:12:07 -0300

    Corrigindo débito técnico

--- src/calc/exceptions/CalcException.java
+++ src/calc/exceptions/CalcException.java
@@ -2,6 +2,7 @@ package calc.exceptions;
 
 public class CalcException extends Exception {
 
+	private static final long serialVersionUID = -7609177080514770993L;
 
 	public CalcException(String mensagem, Exception e) {
 		super(mensagem, e);
--- src/calc/utility/files/GeradorClusterCSV.java
+++ src/calc/utility/files/GeradorClusterCSV.java
@@ -18,7 +18,7 @@ public class GeradorClusterCSV {
 		FileWriter fileWriter = null;
 		try {
 			fileWriter = new FileWriter("clusterGephi.csv");
-			fileWriter.append(FILE_HEADER.toString());
+			fileWriter.append(FILE_HEADER);
 			fileWriter.append(NEW_LINE_SEPARATOR);
 
 			for (int i = 0; i < 10; i++) {
